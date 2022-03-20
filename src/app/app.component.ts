5d2a97a464775be7909a0dcb9762dd7fa046040d
balaji1610 <balajiece1610>
2022-03-21 01:21:23 +0530

    Template Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'form';
+
+  form1 = 'Template driven form';
+
+  form2 = 'Reactive form';
+
+  Getfromdata(data: any) {
+    console.log(data);
+  }
 }
