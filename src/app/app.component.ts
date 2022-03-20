55e8a94addc5f8afcd2555551dc1f0e29271d2eb
balaji1610 <balajiece1610>
2022-03-21 03:13:58 +0530

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +10,41 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'form';
 
-  form1 = 'Template driven form';
+  form1 = 'Template Driven Form';
 
-  form2 = 'Reactive form';
+  form2 = 'Reactive Form';
 
   Getfromdata(data: any) {
     console.log(data);
   }
-}
+  // reactive Form
+  registerV2 = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    pswd: new FormControl(
+      '',
+      Validators.pattern('(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}')
+    ),
+    number: new FormControl('', Validators.minLength(10)),
+  });
+
+  GetfromdataV2() {
+    console.log(this.registerV2.value);
+  }
+
+  get vname() {
+    return this.registerV2.get('name');
+  }
+
+  get vemail() {
+    return this.registerV2.get('email');
+  }
+
+  get vpswd() {
+    return this.registerV2.get('pswd');
+  }
+
+  get vnumber() {
+    return this.registerV2.get('number');
+  }
+} // End
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
